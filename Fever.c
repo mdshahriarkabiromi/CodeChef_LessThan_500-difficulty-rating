cbf58cf26cabbb0d72990f3d780211da53ddf314
mdshahriarkabiromi <shahriar.eu.cse59@gmail.com>
2025-06-09 11:28:27 +0600

    Added 5 problem set 1

---
+++ Fever.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int T;
+	scanf("%d",&T);
+	for(int i=1;i<=T;i++)
+	{
+	    int X;
+	    scanf("%d",&X);
+	    if(X>98)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+    }
+}
---
+++ Sleep_deprivation.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int T;
+	scanf("%d",&T);
+	for(int i=1;i<=T;i++)
+	{
+	    int X;
+	    scanf("%d",&X);
+	    if(7>X)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+    }
+}
